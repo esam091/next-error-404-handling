11cc149367062c204c600d6ec9cd3574abeb1972
Samuel <se12345a@email.me>
2024-08-15 13:17:05 +0700

    readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h2 className="font-semibold">App home page</h2>
-      <p className="mt-2">
+      <p className="mt-2 text-center">
         Use the button in the navigation bar to navigate around
       </p>
     </main>
