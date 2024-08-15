a50039ba827bc627ca1f8660a7990e50e8908353
Samuel <se12345a@email.me>
2024-08-15 13:03:06 +0700

    center text

--- src/app/shop/[id]/not-found.tsx
+++ src/app/shop/[id]/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="w-full flex flex-col items-center p-10">
       <h2 className="font-semibold">Shop not found</h2>
-      <p className="mb-2">This 404 message is only found in the shop page</p>
+      <p className="mb-2 text-center">This 404 message is only found in the shop page</p>
       <Link href='/' className={buttonVariants({})}>Go to home page</Link>
     </div>
   );
